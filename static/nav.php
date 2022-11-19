a0865873bfac4838814851d8124adc31b6cf1627
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-11-19 23:55:10 +0100

    Added selected style to <nav>

--- static/nav.php
+++ static/nav.php
@@ -1,9 +1,31 @@
 <nav>
     <ul>
-        <li><a href="index.php"><img src="img/accueilDark.svg"><p>Accueil</p></a></li>
-        <li><a href="index.php?action=Statistique"><img src="img/statistiqueDark.svg"><p>Statistique</p></a></li>
-        <li><a href="index.php?action=Add"><img src="img/addDark.svg"><p>Ajouter une musique</p></a></li>
-        <li><a href="index.php?action=Playlist"><img src="img/playlistDark.svg"><p>Playlist</p></a></li>
-        <li><a href="index.php?action=Musique"><img src="img/musicalDark.svg"><p>Musique</p></a></li>
+        <?php
+        // verify which page is active
+        $active = '';
+        if (isset($_GET['action'])) {
+            $active = $_GET['action'];
+        }
+        if($active == '')
+            echo '<li><a href="index.php" class="selected"><img src="img/accueil.svg"><p>Accueil</p></a></li>';
+        else
+            echo '<li><a href="index.php"><img src="img/accueilDark.svg"><p>Accueil</p></a></li>';
+        if($active == 'Statistique')
+            echo '<li><a href="index.php?action=Statistique" class="selected"><img src="img/statistique.svg"><p>Statistique</p></a></li>';
+        else
+            echo '<li><a href="index.php?action=Statistique"><img src="img/statistiqueDark.svg"><p>Statistique</p></a></li>';
+        if($active == 'Add')
+            echo '<li><a href="index.php?action=Add" class="selected"><img src="img/add.svg"><p>Ajouter une musique</p></a></li>';
+        else
+            echo '<li><a href="index.php?action=Add"><img src="img/addDark.svg"><p>Ajouter une musique</p></a></li>';
+        if($active == 'Playlist')
+            echo '<li><a href="index.php?action=Playlist" class="selected"><img src="img/playlist.svg"><p>Playlist</p></a></li>';
+        else
+            echo '<li><a href="index.php?action=Playlist"><img src="img/playlistDark.svg"><p>Playlist</p></a></li>';
+        if($active == 'Musique')
+            echo '<li><a href="index.php?action=Musique" class="selected"><img src="img/musical.svg"><p>Musique</p></a></li>';
+        else
+            echo '<li><a href="index.php?action=Musique"><img src="img/musicalDark.svg"><p>Musique</p></a></li>';
+        ?>
     </ul>
 </nav>

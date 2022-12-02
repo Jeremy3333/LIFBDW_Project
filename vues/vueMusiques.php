17d8ac512f0d848ad925a9834cca0e8119522eb7
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-12-02 08:19:35 +0100

    Added musiques logo

--- vues/vueMusiques.php
+++ vues/vueMusiques.php
@@ -3,7 +3,7 @@
         <?php
         foreach($chansons as $chanson)
         {
-            echo "<li><a href='index.php?action=Musique&idC=".$chanson['idC']."'><p>".$chanson["Titre"] . "</p></a></li>";
+            echo "<li><a href='index.php?action=Musique&idC=".$chanson['idC']."'><img src='img/play.svg' /><p>".$chanson["Titre"] . "</p></a></li>";
         }
         ?>
     </ul>

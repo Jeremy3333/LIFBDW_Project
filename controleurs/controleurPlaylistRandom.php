9a87ad2262b6e634e24c37d347550edb76cfda40
Jeremy3333 <jeremy.simond220801@gmail.com>
2022-12-04 15:02:40 +0100

    Fetch back playlist random vue and controler

---
+++ controleurs/controleurPlaylistRandom.php
@@ -0,0 +1,72 @@
+<?php
+function timeToSeconds(string $time)
+{
+    $arr = explode(':', $time);
+    if (count($arr) === 3) {
+        return $arr[0] * 3600 + $arr[1] * 60 + $arr[2];
+    }
+    return $arr[0] * 60 + $arr[1];
+}
+
+
+
+function makeRandomPlaylist($titre, $duree, $genre, $pref)
+{
+    $addduree = 0;
+    $i = 1;
+    $idLL = postListesdeLecture($titre);
+    $timepl = timeToSeconds($duree);
+
+    while ($timepl-60 >= $addduree or $timepl+60 <= $addduree)
+    {
+        if ($genre=='default')
+            $i=4;
+
+        if($i==4)
+        {
+            $versions = getVersionAll();
+            $i=0;
+        }
+        else
+        {
+            $versions = getVersion($genre);
+            $i++;
+        }
+
+        $nbVersion = count($versions);
+        $nb = rand(0,$nbVersion-1);
+        postInclut($idLL,$versions[$nb]['idV']);
+
+        $addduree += timeToSeconds($versions[$nb]['Durée']);
+
+        if($timepl+60 < $addduree)
+        {
+            $versions = getVersionLL($idLL);
+            $nbVersion = count($versions);
+            $nbs = rand(0,$nbVersion-1);
+            deleteInclut($versions[$nbs]['idV'],$idLL);
+            $addduree -= timeToSeconds($versions[$nbs]['Durée']);
+        }
+    }
+
+    echo "<p>".$titre."</p>";
+
+    echo "<p>La playlist dure : ".gmdate("H:i:s",$addduree)."</p>";
+
+    if ($genre!='default')
+        echo "<p>La playlist comorte : ".pourcentageGenre($idLL,$genre)."% du genre ".nomGenre($genre)."</p>";
+}
+
+
+if(isset($_POST['Créer']))
+{
+    $titre = $_POST['titre'];
+    $duree = $_POST['duree'];
+    $genre = $_POST['genre'];
+    $pref = $_POST['preference'];
+    makeRandomPlaylist($titre, $duree, $genre, $pref);
+}
+
+$Genres = getGenres();
+
+?>
---
+++ vues/vuePlaylistRandom.php
@@ -0,0 +1,26 @@
+<div class="Form">
+    <form method="post" action="index.php?action=playlistRandom">
+        <label for="titre">Nom de la playlist</label>
+        <input type="text" name="titre" id="titre" placeholder="Nom de la playlist" require/>
+        <label for="duree">Durée (Bien séparer les informtions par ':' et penser à mettre les 2 chifrres)</label>
+        <input type="text" pattern="([0-5]{1}[0-9]{1}:){0,2}[0-5]{0,1}[0-9]{1}(\.\d+)?" name="duree" id="duree" placeholder="Durée" require/>
+        <label for="genre">Genre</label>
+        <select name="genre" id="genre">
+            <option value="default">Aucun genre priviligié</option>
+            <?php
+            foreach($Genres as $genre)
+            {
+                echo '<option value="'.$genre['idG'].'">'.$genre['Genre'].'</option>';
+            }
+            ?>
+        </select>
+        <label for="preference">Préférence</label>
+        <select name="preference" id="preference">
+            <option value="default">Pas de préférence</option>
+            <option value="playcount">Les plus jouées</option>
+            <option value="skipcount">Les plus sautées</option>
+            <option value="lastplayed">Les plus récentes</option>
+        </select>
+        <input type="submit" name="Créer" value="Créer" />
+    </form>
+</div>
